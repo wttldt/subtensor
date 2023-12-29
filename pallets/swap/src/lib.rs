d000525ee618aa8a8279a7b8c855cded36eb3900
Ayden Brewer <dalegribble@riseup.net>
2023-12-29 13:12:35 -0800

    Add more functions to AssetsInterface

--- pallets/swap/src/lib.rs
+++ pallets/swap/src/lib.rs
@@ -59,12 +59,14 @@ pub struct Pool
 
 pub trait AssetsInterface<Origin, AssetIdParameter, AccountId, Balance> 
 {
-    fn force_create(origin: Origin, id: AssetIdParameter, owner: AccountId, is_sufficient: bool, min_balance: Balance) -> DispatchResult;
-    fn force_set_metadata(origin: Origin, id: AssetIdParameter, name: Vec<u8>, symbol: Vec<u8>, decimals: u8, is_frozen: bool) -> DispatchResult;
     fn start_destroy(origin: Origin, id: AssetIdParameter) -> DispatchResult;
     fn destroy_accounts(origin: Origin, id: AssetIdParameter) -> DispatchResultWithPostInfo;
     fn destroy_approvals(origin: Origin, id: AssetIdParameter) -> DispatchResultWithPostInfo;
     fn finish_destroy(origin: Origin, id: AssetIdParameter) -> DispatchResult;
+	fn mint(origin: Origin, id: AssetIdParameter, beneficiary: AccountId, amount: Balance) -> DispatchResult;
+	fn burn(origin: Origin, id: AssetIdParameter, who: AccountId, amount: Balance) -> DispatchResult;
+	fn balance(id: AssetIdParameter, who: AccountId) -> Balance;
+	fn total_supply(id: AssetIdParameter) -> Balance;
 }
 
 #[frame_support::pallet]
